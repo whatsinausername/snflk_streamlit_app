64b86442083a05973d054b31c3f9fcf8222642de
whatsinausername <55651663+whatsinausername@users.noreply.github.com>
2023-01-04 17:17:39 +0100

    Update streamlit_app.py
    Adding fruit col as index and then letting them to be in the multi select

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,7 @@ streamlit.header('\N{flexed biceps} Breakfast of Champion Coders \N{flexed bicep
 
 ## Importing pandas and panda functios
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
