b4b355e10d268edf468a566a0eb5be3129eff758
whatsinausername <55651663+whatsinausername@users.noreply.github.com>
2023-01-04 17:13:23 +0100

    Update streamlit_app.py
    adding pandas library

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas 
 
 streamlit.title("My test streamlit's display")
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
@@ -8,3 +9,7 @@ streamlit.text('🥑 Kale, Spinach & Rocket Smoothie')
 streamlit.text(' 🍞 Hard-Boiled Free-Range Egg')
 
 streamlit.header('\N{flexed biceps} Breakfast of Champion Coders \N{flexed biceps}')
+
+## Importing pandas and panda functios
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
