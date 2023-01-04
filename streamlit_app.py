babe505471ee67bf6055a51ca22df9f1ad45a189
whatsinausername <55651663+whatsinausername@users.noreply.github.com>
2023-01-04 17:08:09 +0100

    Update streamlit_app.py
    Test add emojis to the text

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title("My test streamlit display")
 
-streamlit.header('Breakfast Menu')
+streamlit.header('🥣 Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥑 Kale, Spinach & Rocket Smoothie')
+streamlit.text(' 🍞 Hard-Boiled Free-Range Egg')
