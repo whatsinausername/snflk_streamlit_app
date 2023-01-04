8fb78b7035fe82a1368be1b948f24971d99fe2db
whatsinausername <55651663+whatsinausername@users.noreply.github.com>
2023-01-04 17:11:17 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,5 @@ streamlit.header('🥣 Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥑 Kale, Spinach & Rocket Smoothie')
 streamlit.text(' 🍞 Hard-Boiled Free-Range Egg')
+
+streamlit.header('\N{flexed biceps} Breakfast of Champion Coders \N{flexed biceps}')
