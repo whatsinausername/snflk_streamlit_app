88d6ccdd2da4597e3bc7138ca3c5c9c462a63a2c
whatsinausername <55651663+whatsinausername@users.noreply.github.com>
2023-01-05 02:28:49 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # display the table
 streamlit.dataframe(fruits_to_show)
 
+streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
 
