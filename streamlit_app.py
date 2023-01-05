7b94c39d59f5c1269c01f30bbd071baa200e0ade
whatsinausername <55651663+whatsinausername@users.noreply.github.com>
2023-01-06 00:56:37 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,7 +59,7 @@ def insert_row_snowflake(new_fruit):
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add')
 
-if streamlit.button.text_input('fruit add'):
+if streamlit.button('fruit add'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     back_from_function = insert_row_snowflake(add_my_fruit)
     streamlit.text(back_from_function)
